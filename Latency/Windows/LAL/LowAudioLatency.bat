b84f9c1ddd030b47e008a88adb163bee38c33fae
Andrew Marchukov <Jeklani@gmail.com>
2026-02-26 19:51:30 +0300

    Add comment to LowAudioLatency.bat

--- Latency/Windows/LAL/LowAudioLatency.bat
+++ Latency/Windows/LAL/LowAudioLatency.bat
@@ -1,2 +1,3 @@
+;https://github.com/spddl/LowAudioLatency
 copy "%~dp0\low_audio_latency_no_console.exe" C:\low_audio_latency_no_console.exe
 schtasks /create /tn "LowAudioLatency" /tr "cmd /c timeout /t 11 & C:\low_audio_latency_no_console.exe" /sc onstart /rl highest /f /ru "SYSTEM"
