7fac9cb259675405e9b73079af18a07f75f654c6
Andrew Marchukov <Jeklani@gmail.com>
2026-03-04 20:18:31 +0300

    Update NetworkTweaks.bat to modify Windows Defender settings
    Added commands to configure Windows Defender settings for UDP connections and telemetry.

--- Network/NetworkTweaks.bat
+++ Network/NetworkTweaks.bat
@@ -1,2 +1,7 @@
 rem # Packet Scheduler Timer Resolution
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Psched" /v "TimerResolution" /t REG_DWORD /d "1" /f
+
+rem # Windows Defender disable inspection of UDP connections.
+Set-MpPreference -DisableDatagramProcessing 1
+rem # Windows Defender disable the gathering and sending of performance telemetry from network protection.
+Set-MpPreference -DisableNetworkProtectionPerfTelemetry 1
