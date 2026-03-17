6cabf2666f068a5f4a100fe2445f56b2cbd105ae
AndrewMarchukov <jeklani@gmail.com>
2026-03-17 17:55:54 +0300

    more

--- Network/NetworkTweaks.bat
+++ Network/NetworkTweaks.bat
@@ -6,7 +6,7 @@ Set-MpPreference -DisableDatagramProcessing 1
 rem # Windows Defender disable the gathering and sending of performance telemetry from network protection.
 Set-MpPreference -DisableNetworkProtectionPerfTelemetry 1
 
-rem # Add TCP settings for BBR2 and ECN
+rem # TCP settings for BBR2 and ECN
 netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2
 netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2
@@ -15,6 +15,9 @@ netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2
 netsh int ipv6 set gl loopbacklargemtu=disable
 netsh int ipv4 set gl loopbacklargemtu=disable
 
-rem # Add TCP settings for DCTCP and ECN if BBR2 not working
+rem # TCP settings for DCTCP and ECN if BBR2 not working
 ::netsh int tcp set supplemental template=Internet congestionprovider=DCTCP
 ::netsh int tcp set global ECN=Enabled
+
+rem #Disable power-saving features
+Disable-NetAdapterPowerManagement -Name "Ethernet"
